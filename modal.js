0a996ca416d92bf1587c0c25397011d64b5ff865
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-07-28 22:05:26 +0200

    ajout de fonction js

--- modal.js
+++ modal.js
@@ -7,23 +7,48 @@ function editNav() {
   }
 }
 
+function validate(){
+  thanks ();
+  hideModal();
+  return false;
+  
+}
+
+function thanks () {
+
+  thanksForResa.style.display ="block";
+}
+
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector(".bground"); // a garder
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const thanksForResa = document.querySelector(".bground2"); // a garder
+const bla = document.querySelector("form");
 
-
-
+bla.addEventListener("submit", validate )
 
 
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach ( function (btn) {
+  console.log(btn);
+  btn.addEventListener("click", launchModal)
+} );
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+// launch modal form hide
+function hideModal() {
+  modalbg.style.display = "none";
+}
+
+//lauch message  btn-submit
+
+//launch message thanks for reservation
 
 
