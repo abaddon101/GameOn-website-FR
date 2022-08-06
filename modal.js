7e398cb9e43e36ba61a2f8a22ef2021b80a7aa4d
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-08-06 17:29:26 +0200

    Refonte totale du Javascript mis en place des fonctions submit formular, events, showmodal,

--- modal.js
+++ modal.js
@@ -1,54 +1,80 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-function validate(){
-  thanks ();
-  hideModal();
-  return false;
-  
-}
-
-function thanks () {
-
-  thanksForResa.style.display ="block";
-}
-
 // DOM Elements
-const modalbg = document.querySelector(".bground"); // a garder
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const thanksForResa = document.querySelector(".bground2"); // a garder
-const bla = document.querySelector("form");
 
-bla.addEventListener("submit", validate )
+// selection the .bground
+let modalBg = document.querySelector(".bground");
+
+// selection the .modal-btn
+let modalBtns = document.querySelectorAll(".modal-btn");
+
+// selection the .formData
+let formData = document.querySelectorAll(".formData");
+
+// selection the .bground2
+let thanksForResa = document.querySelector(".bground2");
+
+// selection the form
+let formEvenenment = document.querySelector("form");
+
+// selecting the submit btn in the formular
+let selectedFormBtn = document.querySelector(".btn-submit");
+
+// closing X
+let closeBtn = document.querySelector(".close");
+
+// close thanks message
+let closeThanksMsg = document.querySelector(".close2");
+
+// DOM Elements End
 
 
 
-// launch modal event
-modalBtn.forEach ( function (btn) {
+
+//close thanks message
+
+
+
+
+
+
+
+
+
+
+// function close modal
+function closeModal(e) {
+  console.log(e);
+  modalBg.style.display = "none";
+}
+
+  // closemodalBg
+  closeBtn.addEventListener("click", closeModal);
+  // closemodalBg End
+
+// function close modal End
+
+// Submit the formular
+formEvenenment.addEventListener("submit", submitFormular);
+
+function submitFormular(e) {
+  e.preventDefault(); // (Working like the return false)
+
+  //close modal
+  closeModal();
+
+  // thanks message
+  thanksForResa.style.display = "block";
+}
+// Submit the formular End
+
+
+// Fonction Events
+
+modalBtns.forEach(function (btn) {
   console.log(btn);
-  btn.addEventListener("click", launchModal)
-} );
+  btn.addEventListener("click", showModal);
+});
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// function showModal() {
+function showModal() {
+  modalBg.style.display = "block";
 }
-
-
-// launch modal form hide
-function hideModal() {
-  modalbg.style.display = "none";
-}
-
-//lauch message  btn-submit
-
-//launch message thanks for reservation
-
-
