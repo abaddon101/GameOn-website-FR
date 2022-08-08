fcd875e9cf79dc8114de65523d5d4950ec078460
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-08-08 12:56:55 +0200

    Ajout de la function // FUNCTION CLOSING THANKS MESSAGE qui permettra la fermeture au clique que ce soit sur la croix ou la fenêtre en elle même du message de remerciement de la reservation

--- modal.js
+++ modal.js
@@ -1,80 +1,111 @@
-// DOM Elements
-
-// selection the .bground
+// DOM ELEMENTS
+/*************************************************************************/
+// SELECTION THE .bground
 let modalBg = document.querySelector(".bground");
+// SELECTION THE .bground END
+/***********************************************************/
 
-// selection the .modal-btn
+// SELECTION THE .modal-btn
 let modalBtns = document.querySelectorAll(".modal-btn");
+// SELECTION THE .modal-btn END
+/*************************************************************************/
 
-// selection the .formData
+// SELECTION THE .formData
 let formData = document.querySelectorAll(".formData");
+// SELECTION THE .formData END
+/*************************************************************************/
 
-// selection the .bground2
+// SELECTION THE .bground2
 let thanksForResa = document.querySelector(".bground2");
+// SELECTION THE .bground2 END
+/*************************************************************************/
 
-// selection the form
+// SELECTION THE form
 let formEvenenment = document.querySelector("form");
+// SELECTION THE form END
+/*************************************************************************/
 
-// selecting the submit btn in the formular
+// SELECTING THE SUBMIT BTN IN THE FORMULAR
 let selectedFormBtn = document.querySelector(".btn-submit");
+// SELECTING THE SUBMIT BTN IN THE FORMULAR END
+/*************************************************************************/
 
-// closing X
+// CLOSING X
 let closeBtn = document.querySelector(".close");
+// CLOSING X END
+/*************************************************************************/
 
-// close thanks message
+// CLOSING THANKS MESSAGE
 let closeThanksMsg = document.querySelector(".close2");
+// CLOSING THANKS MESSAGE END
+/*************************************************************************/
+// DOM ELEMENTS END
 
-// DOM Elements End
+//***********************************************************************
 
+// Fonction Events SHOW MODAL WITH BUTTON
 
-
-
-//close thanks message
-
-
-
-
-
-
-
-
-
-
-// function close modal
-function closeModal(e) {
-  console.log(e);
-  modalBg.style.display = "none";
+// function showModal
+function showModal() {
+  modalBg.style.display = "block";
 }
+// function showModal End
 
-  // closemodalBg
-  closeBtn.addEventListener("click", closeModal);
-  // closemodalBg End
-
-// function close modal End
-
-// Submit the formular
-formEvenenment.addEventListener("submit", submitFormular);
-
-function submitFormular(e) {
-  e.preventDefault(); // (Working like the return false)
-
-  //close modal
-  closeModal();
-
-  // thanks message
-  thanksForResa.style.display = "block";
-}
-// Submit the formular End
-
-
-// Fonction Events
-
+// FUNCTION FOR EACH BUTON
 modalBtns.forEach(function (btn) {
   console.log(btn);
   btn.addEventListener("click", showModal);
 });
+// FUNCTION FOR EACH BUTON END
 
-// function showModal() {
-function showModal() {
-  modalBg.style.display = "block";
+// Fonction Events SHOW MODAL WITH BUTTON END
+
+//***********************************************************************
+
+// SUBMITTING THE FORMULAR
+formEvenenment.addEventListener("submit", submitFormular);
+
+function submitFormular(e) {
+  e.preventDefault(); // (Working like the return false)
+  //close modal
+  closeModal();
+  //close modal End
+
+  // thanks message
+  thanksForResa.style.display = "block";
+  // thanks message End
 }
+// SUBMITTING THE FORMULAR END
+
+// CLOSING modalBg
+closeBtn.addEventListener("click", closeModal);
+// CLOSING modalBg END
+
+// FUNCTION CLOSING MODAL
+function closeModal(e) {
+  console.log(e);
+  modalBg.style.display = "none";
+}
+// FUNCTION CLOSING MODAL END
+
+
+// FUNCTION CLOSING THANKS MESSAGE
+/* When we click on the window or in the X, this window with close it*/
+function hideThanksMessage(){
+  thanksForResa.style.display="none";
+};
+
+thanksForResa.addEventListener("click", hideThanksMessage);
+// FUNCTION CLOSING THANKS MESSAGE END
+
+
+
+
+
+
+
+// FUNCTION CLOSING THANKS MESSAGE END
+
+//***********************************************************************
+
+//***********************************************************************
